90de402fa24d8e1507d14f8ef8a2631d88dc42c5
Luskans <sylvain.migne.pro@gmail.com>
2023-11-08 14:08:16 +0100

    remove proptypes on weather

--- src/component/Weather/Weather.jsx
+++ src/component/Weather/Weather.jsx
@@ -1,11 +1,5 @@
+/* eslint-disable react/prop-types */
 export default function Weather({ datas, onNameChange, onSubmit, cityName }) {
-    // Weather.propTypes = {
-    //     datas: PropTypes.any,
-    //     onNameChange: PropTypes.string.isRequired,
-    //     onSubmit: () => any,
-    //     cityName: PropTypes.string
-    // }
-    
 
     return (
         <>
